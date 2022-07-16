334e03bd48e3ce14f67f64307513eb0ccaacff5a
Evgenii <evgenyi_sit@mail.ru>
2022-07-16 23:49:43 +0300

    modified classes RandomChar, MarvelService

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,18 +1,18 @@
-
-
 class MarvelService {
 
     getResource = async (url) => {
-let res = await fetch(url);
+        let res = await fetch(url);
 
-if(!res.ok){
-    throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
 
-    return await res.json();
-}
+        return await res.json();
     }
 
-    getAllCharacters = () =>{
-        return this.getResource();
+    getAllCharacters = () => {
+        return this.getResource('https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=2cd48c9c7731739aee4b1ac11192ac6d');
     }
 }
+
+export default MarvelService;
